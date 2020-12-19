4b9dd948eebc0c8c375e128ded4b5944ca1ca395
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-12-19 13:30:45 -0600

    adding user api routes and controller methods

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const userRouter = require("./users");
 
-// set routes for different api, user will probably be one
-// EXAMPLE: router.use("/users", require("./users"));
+// set routes for different api
+router.use("/users", require("./users")); // any routes that match "/api/users" go into here
 
 module.exports = router;
