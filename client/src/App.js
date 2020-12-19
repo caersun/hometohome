62db8bd9df6cbeeda2930fb5b06409a0a97fa355
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-12-19 11:17:10 -0600

    why isn't app.js pushing???

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
+// why isn't this showing up :((
 // import Home from "./pages/Home";
 
 function App() {
