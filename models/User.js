69fe3d1cdf7f5a27cb1a79d800ff3b04eeae1834
Toccara Hicks <toccarahicks11@gmail.com>
2021-01-08 13:06:03 -0600

    errors with jest fixed, removed mongoose code, removed token

--- models/User.js
+++ models/User.js
@@ -1,22 +1,4 @@
-const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs'); //hashes the password 
-
-
-const UserSchema = new mongoose.Schema({
-	fullName: String,
-	hash: String,
-	email: { type: String, unique: true, lowercase: true, required: [true, "can't be blank"], index: true }
-});
-
-UserSchema.methods.validPassword = function (password) {
-	return bcrypt.compareSync(password, this.hash);
-};
-
-UserSchema.methods.setPassword = function (password) {
-	this.hash = bcrypt.hashSync(password, 10);
-};
-
-module.exports = mongoose.model('User', UserSchema);
 
 module.exports = (sequelize, DataTypes) => {
 	const User = sequelize.define("User", {
