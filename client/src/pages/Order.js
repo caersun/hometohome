cb16d487459f49f16882d038a67a8bb90389c2c6
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2021-01-09 09:57:03 -0600

    order page

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -1,6 +1,17 @@
 import Component from "react";
-import {} from "reactstrap";
+import { Card, CardImg, CardBody, CardTitle, CardText, Button } from "reactstrap";
 
-class Order extends Component {};
+class Order extends Component {
+    render() {
+        return <Card>
+            <CardImg top width="100%" src={this.props.img} alt={this.props.title}></CardImg>
+            <CardBody>
+                <CardTitle tag="h5">{this.props.title}</CardTitle>
+                <CardText>{this.props.description}</CardText>
+                <Button>Add to Order</Button>
+            </CardBody>
+        </Card>
+    }
+};
 
 export default Order;
