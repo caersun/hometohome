e91ae3febaa3155ae4cce3cec9779f0a96ec27b9
amandaneal1131 <amandaneal1131@gmail.com>
2021-01-11 20:22:59 -0600

    changes to seeds

--- db/seed.sql
+++ db/seed.sql
@@ -3,7 +3,7 @@ USE foodDB;
 INSERT INTO department (name)
 VALUES ('Description'), ('FoodItems'), ('Login'), ('Order'), ('Payments'), ('Scroll');
 
-INSERT INTO role (description_food, food_items)
+INSERT INTO role (description_food)
 VALUES ('Tex Mex and Spansih Cuisine'), ('Italian Cuisine'), ('American Cuisine'), ('Mediterranean Cuisine'), ('Indian Cuisine'), ('Asian Cuisine'), ('Holiday Meals'), ('Desserts'), ('Keto');
 
 INSERT INTO department (food_type, food_item) 
