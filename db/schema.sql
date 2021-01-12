c60f39f10df982fa9617e0965b5b039e351ed220
amandaneal1131 <amandaneal1131@gmail.com>
2021-01-11 20:55:05 -0600

    push temporary schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,31 @@
 DROP DATABASE IF EXISTS hometohome;
+
 CREATE DATABASE hometohome;
+
+USE employeeDB;
+
+CREATE TABLE department
+(
+    id INT
+    auto_increment PRIMARY KEY NOT NULL,
+    name VARCHAR
+    (30) NOT NULL
+);
+
+    CREATE TABLE food_type
+    (
+        id INT
+        auto_increment PRIMARY KEY NOT NULL,
+    food_type VARCHAR
+        (30) NOT NULL,
+);
+
+        CREATE TABLE food_item
+        (
+            id INT
+            auto_increment PRIMARY KEY NOT NULL,
+    food_type VARCHAR
+            (30),
+    food_item VARCHAR
+            (30),
+);
