b5dfa0c7b3086b9d772a46d0bba2b2ac4c6cc42b
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2021-01-11 20:52:26 -0600

    NOW cleaning up server

--- server.js
+++ server.js
@@ -27,59 +27,9 @@ require("./config/passport")(passport);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // const path = require('path');
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  // });
 }
 
 app.use(routes);
-
-// app.post("/login", (req, res, next) => {
-// //   passport.authenticate("local", (err, user, infoCB) => {
-// //     if (!user) res.send("No user exists");
-// //     else {
-// //       req.logIn(user, err => {
-// //         res.send("Successfully authenticated");
-// //         console.log(req.user);
-// //       })
-// //     }
-// //   })
-// // }
-// // (req, res, next) => {
-//   passport.authenticate("local", (err, user, infoCB) => {
-//     // if (err) throw err;
-//     if (!user) res.send("No user exists");
-//     else {
-//       req.logIn(user, err => {
-//         // if (err) throw err;
-//         res.send("Successfully authenticated");
-//         console.log(req.user);
-//       })
-//     }
-//   })(req, res, next); }
-// );
-// app.post("/register", (req, res) => {
-//   console.log("in register route", req.body);
-//   db.User.create({
-//     fullName: req.body.fullName,
-//     email: req.body.email,
-//     hash: req.body.password
-//   });
-// });
-// probably want this route to be /api/users/:id?
-// app.get("/user", (req, res) => {
-//   console.log(req.body);
-// });
-//create a new post to login
-// app.post('/login',
-//   passport.authenticate('local', {
-//       successRedirect: '/',
-//       failureRedirect: '/login'
-//   }
-// ));
-// require('./models/Users');
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hometohome");
 
 // IMPORTANT: { force: true } when changing data structures and upon initialization; else { force: false }
 db.sequelize
