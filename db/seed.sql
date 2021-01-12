381cd0a15ed3e511c72488bc768ec872f001ae5a
amandaneal1131 <amandaneal1131@gmail.com>
2021-01-11 20:12:15 -0600

    add seeds

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE foodDB;
+
+INSERT INTO department (name)
+VALUES ('Description'), ('FoodItems'), ('Login'), ('Order'), ('Payments'), ('Scroll');
+
+INSERT INTO role (description_food, food_items)
+VALUES ('Tex Mex and Spansih Cuisine'), ('Italian Cuisine'), ('American Cuisine'), ('Mediterranean Cuisine'), ('Indian Cuisine'), ('Asian Cuisine'), ('Holiday Meals'), ('Desserts'), ('Keto');
+
+INSERT INTO department (food_type, food_item) 
+VALUES (''), (''), (''), (''), (''), ('');
