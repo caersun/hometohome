5ac9fd212e26475aab930ad45062a6d6f99b2d27
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2021-04-05 12:32:53 -0500

    might have to import

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(sslRedirect());
+app.use(sslRedirect);
 
 // making flash messages available to the front-end
 app.use((req, res, next) => {
